394ed6aef38af14f06cc1258ff6ad73ca5acd092
anupamverma03 <anupamchaudharyr615@gmail.com>
2025-11-19 11:48:46 +0530

    Addinga cpp file

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Hi this is my first readme file.
 <br>
 Author: Anupam Verma
+<br>
+I am Masters student at VNIT Nagpur.
+<br>
+This is a text file.
---
+++ sumTillN.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter a num: ";
+    cin >> n;
+    int sum = 0;
+    for(int i = 0; i <= n; i++){
+        sum += i;
+    }
+    cout << "Sum is " << sum;
+}
